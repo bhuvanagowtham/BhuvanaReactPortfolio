3c10134663cf210698a2dcfa139e76d53953f134
bhuvanagowtham <bhuvi.it05@gmail.com>
2023-02-09 18:49:05 +0530

    First commit for React Portfolio app

---
+++ src/resumeData.js
@@ -0,0 +1,98 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Bhuvaneshwari",
+    "role": "Trainer and Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/bhuvaneshwari-s-61608063",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Surround yourself with people and things that inspire you. Learn everything you can.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/bhuvaneshwari-s-61608063",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/bhuvanagowtham",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    "aboutme":"I deep dived into various technologies like C, C++, Data Structures, HTML, CSS, Javascript, React. I enjoy learning which made me to teach as well. I am a dynamic communicator with good presentation skills. My hobbies are cooking, drawing and stitching.",
+    "address":"Coimbatore",
+    "website":"https://github.com/bhuvanagowtham",
+    "education":[
+      {
+        "UniversityName":"Karpagam Academy of Higher Education",
+        "specialization":"M.E (CSE)",
+        "YearOfPassing":"2013",
+      },
+      {
+        "UniversityName":"Coimbatore Institute of Engg and Tech",
+        "specialization":"IT",
+        "YearOfPassing":"2009",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"iamneo",
+        "specialization":"Senior Skill Development Engineer",
+        "YearOfLeaving":"2022",
+        "Achievements":"Won Neo Precious Gem and Neo Shining Star award for the extraordinary contribution in work"
+      },
+      {
+        "CompanyName":"WhiteHat Junior and Freelancer",
+        "specialization":"Technical Trainer",
+        "YearOfLeaving":"2019",
+        "Achievements":"Received certificates for completing web tech courses and participated in hackathon"
+      },
+      {
+        "CompanyName":"Sri Eshwar College of Engg",
+        "specialization":"Technical Trainer",
+        "YearOfLeaving":"2017",
+        "Achievements":"Trained the students for placements and supported in building projects"
+      },
+      {
+        "CompanyName":"Karpagam College of Engg",
+        "specialization":"Team Lead",
+        "YearOfLeaving":"2013",
+        "Achievements":"Designed courses for industry based curriculum and trained the students"
+      }
+    ],
+    "skillsDescription":"HTML, CSS, Javascript, React, Python, Data Structures",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"Python"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Course Management System",
+        "description":"Webapp",
+        "imgurl":"images/portfolio/cms.png"
+      },
+      {
+        "name":"Rental Service Webapp",
+        "description":"Webapp",
+        "imgurl":"images/portfolio/rental.png"
+      },
+      {
+        "name":"Weather App",
+        "description":"Webapp",  
+        "imgurl":"images/portfolio/weather.png"
+      },
+      
+    ],
+    
+  }
+  
+  export default resumeData
